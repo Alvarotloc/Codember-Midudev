6abc5504861ff37584444ad631e32615e0fb8526
Alvarotloc <alvarotloc@gmail.com>
2022-11-10 10:37:54 +0100

    Primer test pasado

---
+++ src/01-twitter.js
@@ -0,0 +1,5 @@
+const usersFilter = () => {
+
+}
+
+export default usersFilter
--- tests/01-twitter.test.js
+++ tests/01-twitter.test.js
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest'
+import usersFilter from '../src/01-twitter'
 
 describe('Test en reto de twitter de codember', () => {
   it('Should be a function', () => {
