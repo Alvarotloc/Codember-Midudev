903d0a256a7cc17dcb10885ce45e1a278d14c1c4
Alvarotloc <alvarotloc@gmail.com>
2022-11-10 10:37:04 +0100

    Inicio de proyecto, vitest y standard configurados, primer test fallido

---
+++ tests/01-twitter.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('Test en reto de twitter de codember', () => {
+  it('Should be a function', () => {
+    expect(typeof usersFilter).toBe('function')
+  })
+})
