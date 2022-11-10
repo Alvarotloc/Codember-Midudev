30ac52d80e2886b61f8c154aff20c08686f3288a
Alvarotloc <alvarotloc@gmail.com>
2022-11-10 12:31:39 +0100

    Primer reto completado

--- src/01-twitter.js
+++ src/01-twitter.js
@@ -1,5 +1,27 @@
-const usersFilter = () => {
-
+const getUsers = async () => {
+  try {
+    const respuesta = await fetch('https://codember.dev/users.txt')
+    const usuarios = await respuesta.text()
+    const arrayUsuarios = usuarios.split('\n\n')
+    return arrayUsuarios
+  } catch (error) {
+    throw new Error('bad fetch')
+  }
 }
-
-export default usersFilter
+const usersFilter = async () => {
+  const usuarios = await getUsers()
+  let contador = 0
+  let usuarioFinal
+  usuarios.forEach(usuario => {
+    if (usuario.includes('usr:') && usuario.includes('eme:') && usuario.includes('psw:') && usuario.includes('age:') && usuario.includes('loc:') && usuario.includes('fll:')) {
+      contador += 1
+      usuarioFinal = usuario
+    }
+  })
+  return { contador, usuarioFinal }
+}
+console.log(await usersFilter())
+export {
+  getUsers,
+  usersFilter
+}
--- tests/01-twitter.test.js
+++ tests/01-twitter.test.js
@@ -1,8 +1,23 @@
 import { describe, expect, it } from 'vitest'
-import usersFilter from '../src/01-twitter'
+import { usersFilter, getUsers } from '../src/01-twitter'
 
 describe('Test en reto de twitter de codember', () => {
-  it('Should be a function', () => {
+  it('usersFilter should be a function', () => {
     expect(typeof usersFilter).toBe('function')
+  })
+
+  it('getUsers should be a function', () => {
+    expect(typeof getUsers).toBe('function')
+  })
+
+  it('getUsers should return an array made of users, the first one must be midudev and there can be no empty spaces', async () => {
+    const usuarios = await getUsers()
+    expect(Array.isArray(usuarios)).toBe(true)
+    expect(usuarios[0]).toBe('usr:@midudev eme:mi@gmail.com psw:123456 age:38 loc:bcn fll:82')
+    expect(usuarios.includes(' ')).toBe(false)
+  })
+
+  it('usersFilter should return a counter and the last user', async () => {
+    expect(Object.keys(await usersFilter())).toEqual(['contador', 'usuarioFinal'])
   })
 })
